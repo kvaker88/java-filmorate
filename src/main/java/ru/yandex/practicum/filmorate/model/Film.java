67dd92551ddd02d19b4a40c522f34fb081324cc8
Pavel <p.fonyushkin@timeweb.ru>
2025-09-05 13:39:40 +0300

    Исправлены недочёты, указанные в пул реквесте.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,15 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class Film {
     private Long id;
     private String name;
