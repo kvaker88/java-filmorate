75c95eb27c6b0f5eeee2dcddabdf5e6fb8c9df54
Pavel <p.fonyushkin@timeweb.ru>
2025-09-10 16:07:43 +0300

    Убрал лишний импорт.

--- src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.controller.FilmController;
