88c790cfe6b0b7570317ece30bdf0280b60a7332
Arkady465 <155990363+Arkady465@users.noreply.github.com>
2025-09-19 17:29:09 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/InvalidFriendshipException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class InvalidFriendshipException extends RuntimeException {
-    public InvalidFriendshipException(String message) {
-        super(message);
-    }
-}
